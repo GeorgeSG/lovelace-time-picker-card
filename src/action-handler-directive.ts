36732636d004da1da61d6e7aea9f759fce1445f8
Georgi Gardev <georgi@gar.dev>
2024-11-05 22:11:41 +0200

    Update deps

--- src/action-handler-directive.ts
+++ src/action-handler-directive.ts
@@ -168,16 +168,15 @@ class ActionHandler extends HTMLElement implements ActionHandler {
   }
 }
 
-// TODO You need to replace all instances of "action-handler-boilerplate" with "action-handler-<your card name>"
-customElements.define('action-handler-boilerplate', ActionHandler);
+customElements.define('action-handler-time-picker-card', ActionHandler);
 
 const getActionHandler = (): ActionHandler => {
   const body = document.body;
-  if (body.querySelector('action-handler-boilerplate')) {
-    return body.querySelector('action-handler-boilerplate') as ActionHandler;
+  if (body.querySelector('action-handler-time-picker-card')) {
+    return body.querySelector('action-handler-time-picker-card') as ActionHandler;
   }
 
-  const actionhandler = document.createElement('action-handler-boilerplate');
+  const actionhandler = document.createElement('action-handler-time-picker-card');
   body.appendChild(actionhandler);
 
   return actionhandler as ActionHandler;
